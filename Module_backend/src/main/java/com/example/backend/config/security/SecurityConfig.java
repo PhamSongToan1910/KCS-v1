edc3e82b99c5994fbabed006bf2ef1a33a9c75a2
phamsongtoan1910 <85654638+phamsongtoan1910@users.noreply.github.com>
2024-02-28 19:08:22 +0700

    update BE 2802

--- Module_backend/src/main/java/com/example/backend/config/security/SecurityConfig.java
+++ Module_backend/src/main/java/com/example/backend/config/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                                                    UserDetailsService userDetailsService, JwtAuthFilter filter) throws Exception {
         return httpSecurity.csrf().disable()
                 .authorizeHttpRequests()
-                    .requestMatchers("/api/v1/**",  "/api/v1/user/**","/ws/**", "api/v1/file/**").permitAll()
+                    .requestMatchers("/api/v1/auth",  "/api/v1/user/**","/ws/**", "api/v1/file/**").permitAll()
                 .and()
                 .authorizeHttpRequests()
                     .requestMatchers("/api").hasAnyRole("ADMINISTRATOR", "USER")
